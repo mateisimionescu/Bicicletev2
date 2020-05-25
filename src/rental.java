4e31ac5bbdca3c7aa2c99a808a74bd85c0eb90cc
Dragos25 <33931450+Dragos25@users.noreply.github.com>
2020-05-26 02:20:25 +0300

    rental

--- src/rental.java
+++ src/rental.java
@@ -3,45 +3,48 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class rental {
-    private int id_company;
-    private String username;
-    private String password;
-    private String name;
-    private String email;
-    private String phone;
-    private String address;
+    private String id_rental;
+    private int id_user;
+    private int id_bike;
+    private String rent_date;
+    private String rent_hour;
+    private String return_hour;
 
-    public void companyInit() {
+
+    public void rentalInit() {
         System.out.println(this.getClass().getName());
         Scanner input = new Scanner(System.in);
-        System.out.println("Username:");
-        username = input.nextLine();
-        System.out.println("Password:");
-        password = input.nextLine();
-        System.out.println("Name:");
-        name = input.nextLine();
-        System.out.println("E-mail:");
-        email = input.nextLine();
-        System.out.println("Phone:");
-        phone = input.nextLine();
-        System.out.println("Address:");
-        address = input.nextLine();
+        System.out.println("id_user:");
+        id_user = input.nextInt();
+        System.out.println("id_bike:");
+        id_bike = input.nextInt();
+        System.out.println("Rent date:");
+        input.nextLine();
+        rent_date = input.nextLine();
+        System.out.println("Rent hour:");
+        rent_hour = input.nextLine();
+        System.out.println("Return hour:");
+        return_hour= input.nextLine();
+        String rentalname="U"+id_user+"B"+id_bike+"D"+rent_date;
+        id_rental=rentalname;
+
     }
 
     public void insert() {
         System.out.println("Inserting records into the table...");
 
         try {
-            String query = " insert into company (username, password, name, email, phone, address)"
-                    + " values (?, ?, ?, ?, ?, ?)";
+            String query = " insert into rental (id_rental,id_user, id_bike, rent_date, rent_hour, return_hour)"
+                    + " values (?,?, ?, ?, ?, ?)";
 
             PreparedStatement preparedStmt = DBconn.getConnection().prepareStatement(query);
-            preparedStmt.setString (1, username);
-            preparedStmt.setString (2, password);
-            preparedStmt.setString (3, name);
-            preparedStmt.setString (4, email);
-            preparedStmt.setString (5, phone);
-            preparedStmt.setString (6, address);
+            preparedStmt.setString(1,id_rental);
+            preparedStmt.setInt (2, id_user);
+            preparedStmt.setInt (3, id_bike);
+            preparedStmt.setString (4, rent_date);
+            preparedStmt.setString (5, rent_hour);
+            preparedStmt.setString (6, return_hour);
+
 
             preparedStmt.execute();
 
