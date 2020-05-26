066a0457144aee065de3a412766fd0c27b2a936e
Dragos25 <33931450+Dragos25@users.noreply.github.com>
2020-05-26 13:21:15 +0300

    registeru

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import inc.conn.DBconn;
 import inc.gui.LoginGUI;
+import inc.gui.RegisterU;
 
 public class Main {
 
     public static void main(String[] args) {
-        LoginGUI login = new LoginGUI();
+        RegisterU register=new RegisterU();
+        //LoginGUI login = new LoginGUI();
     }
 }
